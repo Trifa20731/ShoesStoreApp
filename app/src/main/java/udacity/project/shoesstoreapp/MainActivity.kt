2eccecb926a428a1e4ba07624f51022f27c20459
Trifa20731 <54383330+Trifa20731@users.noreply.github.com>
2021-09-23 00:44:00 +0800

    Fix the launch problem in main activity

--- app/src/main/java/udacity/project/shoesstoreapp/MainActivity.kt
+++ app/src/main/java/udacity/project/shoesstoreapp/MainActivity.kt
@@ -2,18 +2,27 @@ package udacity.project.shoesstoreapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import udacity.project.shoesstoreapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        navController = this.findNavController(R.id.myNavHostFragment)
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        // TODO: Fix the Fragment Showing Problem
+        navHostFragment = this.supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
